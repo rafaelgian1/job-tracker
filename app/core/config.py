2f5cee965417b1d2d1ee2b01ac4e9caad6f550da
rafael <rafaelgiannopoulos@gmail.com>
2026-08-06 17:16:33 +0300

    Read .env file with Settings class using pydantic

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env',env_file_encoding = 'utf-8')
+    #case insensitive, match with env variables 
+    postgres_user: str 
+    postgres_password: str
+    postgres_db: str
+
+#pydantic will automatically read the values from the .env file
+#and populate the attributes of the Settings class based on the variable names 
+#and their type of the attributes.
